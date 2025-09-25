b4c80eabfb6431609154918694e2137690e0f5ef
barkbuff <connect.arindamhore@gmail.com>
2025-09-26 01:11:38 +0530

    Revise sandbox structure with Starter file kit

---
+++ StarterFile/sandbox.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vi vector<int>
+#define vll vector<long long int>
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt", "r", stdin);
+        freopen("out.txt", "r", stdout);
+    #endif
+
+    std::ios::sync_with_stdio(false);
+}
